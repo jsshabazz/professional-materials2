c417c47e2e03acff15cda0b1231f8a211e136e93
Jamall Shabazz <jsshabazz510@gmail.com>
2021-05-12 10:51:27 -0500

    added css

---
+++ jshaka2.js
@@ -0,0 +1,77 @@
+
+(function(e, t) {
+    'object' == typeof exports && 'undefined' != typeof module ? module.exports = t() : 'function' == typeof define && define.amd ? define(t) : e.Popper = t()
+})(this, function() {
+    'use strict';
+
+    function e(e) {
+        return e && '[object Function]' === {}.toString.call(e)
+    }
+
+    function t(e, t) {
+        if (1 !== e.nodeType) return [];
+        var o = window.getComputedStyle(e, null);
+        return t ? o[t] : o
+    }
+
+    function o(e) {
+        return 'HTML' === e.nodeName ? e : e.parentNode || e.host
+    }
+
+    function n(e) {
+        if (!e || -1 !== ['HTML', 'BODY', '#document'].indexOf(e.nodeName)) return window.document.body;
+        var i = t(e),
+            r = i.overflow,
+            p = i.overflowX,
+            s = i.overflowY;
+        return /(auto|scroll)/.test(r + s + p) ? e : n(o(e))
+    }
+
+    
+   
+
+    function u(e, o) {
+        var i = ie(),
+            r = 'HTML' === o.nodeName,
+            p = g(e),
+            s = g(o),
+            d = n(e),
+            a = t(o),
+            l = +a.borderTopWidth.split('px')[0],
+            m = +a.borderLeftWidth.split('px')[0],
+            h = c({
+                top: p.top - s.top - l,
+                left: p.left - s.left - m,
+                width: p.width,
+                height: p.height
+            });
+        if (h.marginTop = 0, h.marginLeft = 0, !i && r) {
+            var u = +a.marginTop.split('px')[0],
+                b = +a.marginLeft.split('px')[0];
+            h.top -= l - u, h.bottom -= l - u, h.left -= m - b, h.right -= m - b, h.marginTop = u, h.marginLeft = b
+        }
+        return (i ? o.contains(d) : o === d && 'BODY' !== d.nodeName) && (h = f(h, o)), h
+    }
+
+    function b(e) {
+        var t = window.document.documentElement,
+            o = u(e, t),
+            i = _(t.clientWidth, window.innerWidth || 0),
+            n = _(t.clientHeight, window.innerHeight || 0),
+            r = a(t),
+            p = a(t, 'left'),
+            s = {
+                top: r - o.top + o.marginTop,
+                left: p - o.left + o.marginLeft,
+                width: i,
+                height: n
+            };
+        return c(s)
+    }
+
+    function y(e) {
+        var i = e.nodeName;
+        return 'BODY' === i || 'HTML' === i ? !1 : 'fixed' === t(e, 'position') || y(o(e))
+    };
+}
+)
